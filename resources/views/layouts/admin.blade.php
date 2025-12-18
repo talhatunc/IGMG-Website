31d4d8ebd5a3bd41ca83f937bc0920cfb1fa1b4e
talhatunc <muhammedtalhatunc@yandex.com>
2025-12-19 00:37:05 +0300

    feat: Add initial admin panel layout and favicon.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,6 +4,7 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>IGMG Admin Panel | @yield('title', 'Dashboard')</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <!--begin::Accessibility Meta Tags-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
     <meta name="color-scheme" content="light dark" />
